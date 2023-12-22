cf86248dc37e1ea6187ef9eb7c1bbe87bdd8cc4d
Ben Agricola <ben+git@agrico.la>
2023-12-22 09:47:46 +0000

    Add release workflow
    Signed-off-by: Ben Agricola <ben+git@agrico.la>

--- dist/release.sh
+++ dist/release.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 WD="${PWD}"
-echo "Building release zip..."
-TMPDIR=$(mktemp -d -t mos-release-XXXXX)
+TMP_DIR=$(mktemp -d -t mos-release-XXXXX)
+ZIP_NAME="${1:-mos-release}.zip"
 SYNC_CMD="rsync -a --exclude=README.md"
 
+echo "Building release ${ZIP_NAME}..."
+
 # Make stub folder-structure
-mkdir -p ${TMPDIR}/{sys,macros,sys/mos}
+mkdir -p ${TMP_DIR}/{sys,macros,sys/mos}
 
 # Copy files to correct location in temp dir
-${SYNC_CMD} sys/* ${TMPDIR}/sys/
-${SYNC_CMD} macro/public/* ${TMPDIR}/macros/
-${SYNC_CMD} macro/private/* ${TMPDIR}/sys/mos
-${SYNC_CMD} macro/machine/* ${TMPDIR}/sys/
-${SYNC_CMD} macro/movement/* ${TMPDIR}/sys/
+${SYNC_CMD} sys/* ${TMP_DIR}/sys/
+${SYNC_CMD} macro/public/* ${TMP_DIR}/macros/
+${SYNC_CMD} macro/private/* ${TMP_DIR}/sys/mos
+${SYNC_CMD} macro/machine/* ${TMP_DIR}/sys/
+${SYNC_CMD} macro/movement/* ${TMP_DIR}/sys/
 
-find ${TMPDIR}
+find ${TMP_DIR}
 
-[[ -f mos-release.zip ]] && rm mos-release.zip
+[[ -f ${ZIP_NAME} ]] && rm ${ZIP_NAME}
 
-cd "${TMPDIR}" && mv sys/daemon.g sys/daemon.install && zip -x 'README.md' -r "${WD}/mos-release.zip" * && cd "${WD}"
+cd "${TMP_DIR}" && mv sys/daemon.g sys/daemon.install && zip -x 'README.md' -r "${WD}/${ZIP_NAME}" * && cd "${WD}"
 
-rm -rf "${TMPDIR}"
+rm -rf "${TMP_DIR}"
