d642a210f901cbd210b6ce6dcfe5cbe7beed1808
Ben Agricola <ben+git@agrico.la>
2024-02-14 06:18:23 +0000

    Make sure post versions are updated by dist
    We need to make sure post and MillenniumOS versions match, so the post
    needs to have a version string in it. We now replace the version string
    using the same method used to generate the MOS zip file, and output it
    as a release asset.
    
    Signed-off-by: Ben Agricola <ben+git@agrico.la>

--- dist/release.sh
+++ dist/release.sh
@@ -3,12 +3,12 @@ WD="${PWD}"
 TMP_DIR=$(mktemp -d -t mos-release-XXXXX)
 ZIP_NAME="${1:-mos-release}.zip"
 SYNC_CMD="rsync -a --exclude=README.md"
-COMMIT_ID=$(git describe --tags --always --dirty)
+COMMIT_ID=$(git describe --tags --exclude "release-*" --always --dirty)
 
 echo "Building release ${ZIP_NAME} for ${COMMIT_ID}..."
 
 # Make stub folder-structure
-mkdir -p ${TMP_DIR}/{sys,macros,sys/mos}
+mkdir -p ${TMP_DIR}/{sys,macros,sys/mos,posts}
 
 # Copy files to correct location in temp dir
 ${SYNC_CMD} sys/* ${TMP_DIR}/sys/
@@ -17,6 +17,7 @@ ${SYNC_CMD} macro/private/* ${TMP_DIR}/sys/mos
 ${SYNC_CMD} macro/machine/* ${TMP_DIR}/sys/
 ${SYNC_CMD} macro/movement/* ${TMP_DIR}/sys/
 ${SYNC_CMD} macro/tool-change/* ${TMP_DIR}/sys/
+${SYNC_CMD} post-processors/**/* ${TMP_DIR}/posts/
 
 find ${TMP_DIR}
 
@@ -24,8 +25,9 @@ find ${TMP_DIR}
 
 cd "${TMP_DIR}" && {
     mv sys/daemon.g sys/daemon.install &&
-    sed -i -e "s/%%MOS_VERSION%%/${COMMIT_ID}/" sys/mos.g
-    zip -x 'README.md' -r "${WD}/${ZIP_NAME}" * &&
+    sed -i -e "s/%%MOS_VERSION%%/${COMMIT_ID}/" {sys/mos.g,posts/*} &&
+    cp posts/* "${WD}/dist" &&
+    zip -x 'README.md' -x 'posts/' -x 'posts/**' -r "${WD}/dist/${ZIP_NAME}" * &&
     cd "${WD}"
 }
 
